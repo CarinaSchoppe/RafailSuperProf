5e904b690ad36b0fc204d819fa3c4705b3eb2913
Carina Sophie Schoppe <carina.schoppe2912@gmail.com>
2022-12-30 14:12:39 +0100

    commit

--- src/MinusEins.java
+++ src/MinusEins.java
@@ -1,2 +1,25 @@
 public class MinusEins {
+
+    public static int zahlMinus1MitFragezeichen(int zahl) {
+        return zahl == 0 ?
+                0 : zahl < 10 ?
+                //                                      54321 -> 43210
+                zahl - 1 : Integer.parseInt(zahlMinus1MitFragezeichen(zahl / 10) + String.valueOf(zahlMinus1MitFragezeichen(zahl % 10)));
+
+    }
+
+    //abc -> a-1b-1c-1
+
+    public static int zahlMinus1(int zahl) {
+
+        var resText = String.valueOf(zahl);
+
+        var current = Integer.parseInt(resText.substring(0, 1)) - 1;
+        var next = resText.substring(1);
+        if (next.length() == 0) {
+            return current;
+        }
+
+        return Integer.parseInt("" + current + zahlMinus1(Integer.parseInt(next)));
+    }
 }
